e7912a0096fdeffa0c1e13f856ed6fcfc5aece65
glutengo <markus@glutting.net>
2017-03-31 01:04:25 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app works!';
+}) 
+export class AppComponent implements OnInit {
+  @ViewChild('sidenav') sidenav;
+  title = 'Face Recogintion';
+  user;
+
+  constructor(private router: Router, private auth: AuthService) {}
+
+  ngOnInit() {
+    // get user from auth service
+    this.user = this.auth.getUser();
+
+    // subscribe to future changes on user
+    this.auth.user.subscribe(data => {
+      console.log(data);
+      this.user = data
+    });
+  }
+
+  // naveigate to given route and toggle sidebar
+  navigateTo(to: [any]) {
+    this.sidenav.toggle();
+    this.router.navigate(to);
+  }
+
+  // perform logout and toggle sidebar
+  logout() {
+    this.auth.logout();
+    this.sidenav.toggle();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,67 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material'
+import { XHRBackend, RequestOptions } from '@angular/http';
+import { MdSnackBar } from '@angular/material';
+import { LocalStorageModule } from 'angular-2-local-storage';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { routing } from './routing/routing.module';
+import { HomeComponent } from './home/home.component';
+
+import { provideInterceptorService, InterceptorService } from 'ng2-interceptors';
+import { httpInterceptor } from './http-interceptor.service';
+import { AuthService } from './auth.service';
+import { CameraSnapshotComponent } from './camera-snapshot/camera-snapshot.component';
+import { ImagePickerComponent } from './image-picker/image-picker.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UpdateImageComponent } from './update-image/update-image.component';
+import { UpdatePasswordComponent } from './update-password/update-password.component';
+
+export function interceptorFactory(xhrBackend: XHRBackend, requestOptions: RequestOptions, authService, snackbar){
+  let service = new InterceptorService(xhrBackend, requestOptions);
+  service.addInterceptor(new httpInterceptor(authService, snackbar));
+  // Add interceptors here with service.addInterceptor(interceptor)
+  return service;
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ 
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    CameraSnapshotComponent,
+    ImagePickerComponent,
+    UpdateImageComponent,
+    UpdatePasswordComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    routing,
+    BrowserAnimationsModule,
+    LocalStorageModule.withConfig({
+            prefix: 'faceRecognition',
+            storageType: 'localStorage'
+        })
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    {
+      provide: InterceptorService,
+      useFactory: interceptorFactory,
+      deps: [XHRBackend, RequestOptions, AuthService, MdSnackBar]
+    }
+    /*provideInterceptorService([
+      httpInterceptor])*/
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
